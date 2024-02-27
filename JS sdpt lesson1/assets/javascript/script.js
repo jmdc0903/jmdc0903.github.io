ccafa8740a6558d9583b070ba810a49c08311f36
jmdc0903 <johnmichaeldelacruz0903@gmail.com>
2024-02-27 20:38:54 +0800

    Updated Files

---
+++ JS sdpt lesson1/assets/javascript/script.js
@@ -0,0 +1,71 @@
+// $name = "Michael Dela Cruz";
+// document.getElementById("text").innerHTML = $name;
+// console.log($name);
+// alert($name);
+
+// const $name = "Michael Dela Cruz";
+// let $name = "Michael Dela Cruz";
+// alert($name);
+// $name = "Dela Cruz John Michael";
+// alert($name);
+// const _pi = 3.14;
+// alert(_pi);
+
+// const isCoding = true;
+// const isCoding = undefined;
+// const isCoding = null;
+// alert(isCoding);
+
+// CONCATENATION
+// "Hello" + "Wrold"
+// "Answer: " + 12
+// "PI: " + 3.14
+
+// let name = "Michael";
+// console.log("Hi, " + name);
+
+// alert("Hello "+"World");
+
+// let firstname = "Michael";
+// alert(firstname);
+
+// let firstname = "Michael";
+// let pi = 3.14;
+// alert("Hi, " + firstname);
+// alert("How are you doing " + firstname + "?");
+// alert(firstname + ", The value of pi is " + pi);
+
+// let name1 = "Alenere";
+// let name2 = "David";
+// let gender = "she";
+// let drinks = "coffee";
+
+// alert(name1+"," + " the friendly neighbor, waved at " +name2 + " as " + gender + " walked by " + name1+"'s house.");
+// alert(name1+ " smiled back and invited " + name2 + " in for a cup of " + drinks+".");
+
+// Data type = Number, 2 types of NUMBER= Integer(WholeNumber) and Float(DecimalNumber)
+// convert strings to number (integer)= let num = parseInt("5");
+// convert strings to number (decimal)= let pi = parseFloat("3.14");
+
+
+
+let num1 = 5;
+let num2 = 2;
+
+// Addition 
+alert(num1+num2); 
+
+// Subtraction
+alert(num1-num2);
+
+//Multiplication
+alert(num1 * num2);
+
+// Division
+alert(num1 / num2);
+// Modulus
+alert(num1 % num2);
+
+//Exponent
+alert(num1 ** num2);
+
